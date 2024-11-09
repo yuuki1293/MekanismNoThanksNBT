c78aa81730c5b795045693997d7943cbb3c7bd3c
yuuki1293 <yuuki1293@outlook.jp>
2024-11-09 18:23:34 +0900

    fix config not correct

--- src/main/java/yuuki1293/mekanism_ntn/MekanismNTN.java
+++ src/main/java/yuuki1293/mekanism_ntn/MekanismNTN.java
@@ -2,20 +2,22 @@ package yuuki1293.mekanism_ntn;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MekanismNTN.MODID)
-public class MekanismNTN
-{
+public class MekanismNTN {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "mekanism_ntn";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public MekanismNTN()
-    {
+    public MekanismNTN() {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, MekanismNTNConfig.builder.build());
+        LOGGER.debug("Mekanism NTN config loaded");
     }
 }
---
+++ src/main/java/yuuki1293/mekanism_ntn/MekanismNTNConfig.java
@@ -0,0 +1,44 @@
+package yuuki1293.mekanism_ntn;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MekanismNTNConfig {
+    private static final List<String> defaultWhiteList = List.of();
+    private static final List<String> defaultBlackList = Arrays.asList(
+        "basic_bin", "advanced_bin", "elite_bin", "ultimate_bin", "creative_bin", // All Bins
+        "basic_induction_cell", "advanced_induction_cell", "elite_induction_cell", "ultimate_induction_cell", // All Induction Cells
+        "digital_miner", // Digital Miner
+        "personal_barrel", "personal_chest", // Personal Barrel and Chest
+        "logistical_sorter", // Logistical Sorter
+        "basic_fluid_tank", "advanced_fluid_tank", "elite_fluid_tank", "ultimate_fluid_tank", "creative_fluid_tank", // All Fluid Tanks
+        "basic_energy_cube", "advanced_energy_cube", "elite_energy_cube", "ultimate_energy_cube", "creative_energy_cube", // All Energy Cubes
+        "basic_chemical_tank", "advanced_chemical_tank", "elite_chemical_tank", "ultimate_chemical_tank", "creative_chemical_tank", // All Chemical Tanks
+        "cardboard_box" // CardBoard Box
+    );
+
+    public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec.BooleanValue Enabled;
+    public static final ForgeConfigSpec.BooleanValue DefaultEnabled;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> Whitelist;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> Blacklist;
+
+    static {
+        builder.comment("This is Mekanism No Thanks NBT Config");
+        builder.push("general");
+
+        // region [general]
+        Enabled = builder.comment("master switch. default = true")
+            .define("enabled", true);
+        DefaultEnabled = builder.comment("default mode. default = true")
+            .define("defaultEnabled", true);
+        Whitelist = builder.comment("target block whitelist.")
+            .defineList("whitelist", defaultWhiteList, element -> true);
+        Blacklist = builder.comment("target block blacklist.")
+            .defineList("blacklist", defaultBlackList, element -> true);
+        builder.pop();
+        // endregion [general]
+    }
+}
